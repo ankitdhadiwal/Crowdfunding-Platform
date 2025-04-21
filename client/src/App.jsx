50d5823bd239eaf855e3cbaaa7d870307ecc8461
Ankit Dhadiwal <103865323+ankitdhadiwal@users.noreply.github.com>
2025-04-22 01:37:46 +0530

    Completed the navbar and responsive

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,10 @@ const App = () => {
 
                 <Routes>
                     <Route path='/' element={<Home />} />
-                </Routes>
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/create-campaign" element={<CreateCampaign />} />
+                    <Route path="/campaign-details/:id" element={<CampaignDetails />} />
+                 </Routes>
             </div>
         </div>
     )
--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import {ethers} from 'ethers';
+
+import {money} from "../assets";
+import { CustomButton } from '../components';
+import {checkIfImage} from '../utils';
+
 
 const CreateCampaign = () => {
     return (
